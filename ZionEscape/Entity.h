e7c5494dc1a2c1244f9c97ffc1b8cba3ee676b07
FrowsyFrog <frowsyfrog@gmail.com>
2020-11-10 13:15:31 -0500

    Added Code

--- ZionEscape/Entity.h
+++ ZionEscape/Entity.h
@@ -1,1 +1,50 @@
 #pragma once
+#include "Sprite.h"
+using namespace System;
+
+ref class Entity : public Sprite {
+protected:
+  String^ name;
+  int x, y;
+  bool movable;
+  float health;
+  float damagePoints;
+public:
+  Entity(String^ name, Point pos, bool movable, float health, float damagePoints) {
+    this->name = name;
+    this->x = pos.X;
+    this->y = pos.Y;
+    this->movable = movable;
+    this->health = health;
+    this->damagePoints = damagePoints;
+  }
+  ~Entity() {}
+  bool IsMovable() {
+    return this->movable;
+  }
+  float GetHealth() {
+    return this->health;
+  }
+  void SetHealth(float health) {
+    this->health = health;
+  }
+  float GetDamagePoint() {
+    return this->damagePoints;
+  }
+  bool Collision(Entity object) {
+    //Debe ser drawingArea protected
+    //return this->drawingArea.IntersectsWith(object.drawingArea);
+  }
+  virtual void Move(short dx, short dy) {
+    //x y no están enlazados con la posicion de drawingArea
+    this->x += dx;
+    this->y += dy;
+    /*
+    * Una posibilidad podría hacer esto
+    this->drawingArea.X += dx;
+    this->drawingArea.Y += dy;
+    * O también colocar el x y en sprite
+    */
+  }
+};
+
--- ZionEscape/Sprite.h
+++ ZionEscape/Sprite.h
@@ -1,1 +1,36 @@
 #pragma once
+using namespace System;
+using namespace System::Drawing;
+
+ref class Sprite {
+  Bitmap^ image;
+  short col, row, nCols, nRows;
+  Rectangle drawingArea;
+public:
+  void SetPosition(Point point) {
+    this->drawingArea.Location = point;
+  }
+  Point GetPosition() {
+    return drawingArea.Location;
+  }
+  void SetCol(short index) {
+    this->col = index;
+  }
+  void SetRow(short index) {
+    this->row = index;
+  }
+  void Draw(Graphics^ world) {
+    world->DrawImage(image, this->drawingArea, this->GetCrop(), GraphicsUnit::Pixel);
+    //Se debe controlar en cuáles casos animar el Sprite. Tanto por si está en movimiento o por si se trata de algún Obstáculo.
+    this->col = (this->col + 1) % nCols;
+  }
+  Rectangle GetCrop() {
+    short width = this->image->Width / this->nCols;
+    short height = this->image->Height / this->nRows;
+    short x = this->col * width;
+    short y = this->row * height;
+
+    return Rectangle(x, y, width, height);
+  }
+
+};
