761caf64af1f6433301b93f6ee92277583ca5667
Diego Albitres <diego.albitres@gmail.com>
2021-05-04 18:10:42 -0500

    Rewritten FileManager to use System:IO

--- ZionEscape/FileManager.h
+++ ZionEscape/FileManager.h
@@ -10,35 +10,24 @@ using namespace System::Diagnostics;
 ref class FileManager {
 public:
   static void SaveSeed(const int seed) {
-    std::string path = GetFilePath(std::string("saved_seed.txt"));
-    std::fstream file(path, std::ios::out);
-    if (file.is_open()) {
-      file << seed << "\n";
-      file.close();
-    }
+    String^ path = GetFilePath("saved_seed.txt");
+    File::WriteAllText(path, seed.ToString());
   }
 
   static int LoadSeed() {
-    std::string path = GetFilePath(std::string("saved_seed.txt"));
-    std::fstream file(path, std::ios::in);
-    int num = 0;
-    if (file.is_open()) {
-      std::string seed;
-      file >> seed;
-      try {
-        num = std::stoi(seed);
-      } catch (const std::out_of_range&) {}
-      file.close();
+    String^ path = GetFilePath("saved_seed.txt");
+    int seed = 0;
+    if (File::Exists(path)) {
+      String^ text = File::ReadAllText(path);
+      Int32::TryParse(text, seed);
     }
-    return num;
+    return seed;
   }
 
   static void OpenSeedFile() {
     String^ path = GetFilePath("saved_seed.txt");
-    std::string nativePath = StringExtensions::ToStdString(path);
     if (!File::Exists(path)) {
-      std::fstream file(nativePath, std::ios::out);
-      file.close();
+      File::WriteAllText(path, "");
     }
     Process^ fileopener = gcnew Process();
     fileopener->StartInfo->FileName = "explorer.exe";
@@ -47,13 +36,6 @@ public:
   }
 
 private:
-  static std::string GetFilePath(std::string str) {
-    if (str.empty()) {
-      return "";
-    }
-    return StringExtensions::ToStdString(Application::StartupPath) + "\\" + str;
-  }
-
   static String^ GetFilePath(String^ str) {
     if (String::IsNullOrEmpty(str)) {
       return String::Empty;
